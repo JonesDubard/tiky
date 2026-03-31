e1a5b21bf76da9c946a1149b53a1934a9a30fd39
Tiky <jonesdubard1@gmail.com>
2026-03-31 13:37:37 +0000

    Logo Added, Description added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 ﻿import { Providers } from "./providers";
 import "./globals.css";
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-  title: 'Tiky – Events & Polls in Liberia',
-  description: 'Discover and manage events, create polls, and engage with your audience using Tiky, the premier event platform for Liberia.',
+  title: 'Tiky Events & Polls in Liberia',
+  description: 'Discover and manage events in Liberia, create polls, and engage with your audience using Tiky, the premier event platform for Liberia. Whether you\'re an event organizer or an attendee, Tiky offers a seamless experience to connect, share, and participate in events across Liberia. Join Tiky today and be part of the vibrant event community in Liberia!',
 }
 
 export default function RootLayout({
